7d27b2322114eb21c90ea2388dd368c2219a9f71
Louis Quinnell <_@ziorobo.co>
2021-09-27 04:46:35 +1000

    Add `withGenerators` interface

--- begat/core/index.spec.ts
+++ begat/core/index.spec.ts
@@ -1,5 +1,21 @@
-import { thing } from "."
+import { expectType } from "ts-expect"
+import { withDependencies, withGenerators } from "."
 
-it(`works`, () => {
-  expect(thing).toBeTruthy()
+const fnOne = (_: { one: 1 }) => {}
+const fnTwo = (_: { one: 1; two: 2; three?: 3 }) => {}
+
+describe(withDependencies.name, () => {
+  it(`has the expected type`, () => {
+    expectType<{ withConfig: (config: { one: 1; two: 2; three?: 3 }) => void }>(
+      withDependencies({ process }).withGenerators([fnOne, fnTwo])
+    )
+  })
+})
+
+describe(withGenerators.name, () => {
+  it(`has the expected type`, () => {
+    expectType<{ withConfig: (config: { one: 1; two: 2; three?: 3 }) => void }>(
+      withGenerators([fnOne, fnTwo])
+    )
+  })
 })
--- begat/core/index.ts
+++ begat/core/index.ts
@@ -1,1 +1,20 @@
-export const thing: string = "📖"
+import { Union } from "ts-toolbelt"
+
+export type Generator<C = {}> = (config: C) => void
+
+/** Union of config properties from a list of generator functions. */
+type ConfigUnion<Gs extends Generator<any>[]> = Union.IntersectOf<
+  Parameters<Gs[number]>[0]
+>
+
+type Dependencies = {
+  process: typeof process
+}
+
+export const withDependencies = (dependencies: Dependencies) => ({
+  withGenerators: <Gs extends Generator<any>[]>(generators: Gs) => ({
+    withConfig: function (config: ConfigUnion<Gs>) {},
+  }),
+})
+
+export const { withGenerators } = withDependencies({ process })
