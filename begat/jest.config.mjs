f6f5a57fa118cf8eda0825d76841cc757884e4a5
Louis Quinnell <_@ziorobo.co>
2021-09-26 19:21:04 +1000

    Initial commit

---
+++ begat/jest.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * @type {import("@jest/types").Config.InitialOptions}
+ */
+const config = {
+  moduleFileExtensions: ["ts", "js", "json"],
+  preset: "ts-jest",
+  roots: ["<rootDir>/core"],
+  testEnvironment: "node",
+
+  globals: {
+    "ts-jest": {
+      diagnostics: { ignoreCodes: [151001] },
+      useESM: true,
+    },
+  },
+}
+
+export default config
