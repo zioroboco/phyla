fc520c3d26d11d36511a3c9828f41310fe024e94
Louis Quinnell <_@ziorobo.co>
2021-09-29 12:18:42 +1000

    Create `begat-template-eta` package

---
+++ contrib/template-eta/index.spec.ts
@@ -0,0 +1,5 @@
+import { thing } from "."
+
+it(`works`, () => {
+  expect(thing).toBeTruthy()
+})
---
+++ contrib/template-eta/jest.config.mjs
@@ -0,0 +1,19 @@
+/**
+ * @type {import("@jest/types").Config.InitialOptions}
+ */
+const config = {
+  moduleFileExtensions: ["ts", "js", "json"],
+  preset: "ts-jest",
+  roots: ["<rootDir>"],
+  testEnvironment: "node",
+
+  globals: {
+    "ts-jest": {
+      diagnostics: { ignoreCodes: [151001] },
+      tsconfig: "tsconfig.json",
+      useESM: true,
+    },
+  },
+}
+
+export default config
