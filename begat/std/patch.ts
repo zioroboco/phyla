e7490d66f0cf52a693158aae6fb16e91f8229cc9
Louis Quinnell <_@ziorobo.co>
2021-10-06 22:50:36 +1100

    Fix process ending while shelled out to git

--- begat/std/patch.ts
+++ begat/std/patch.ts
@@ -1,17 +1,21 @@
 import { Context, fsFromVolume } from "begat/core/api"
-import { resolve } from "path"
 import { spawn } from "child_process"
 import { sync } from "begat/core/sync"
-import { withDir } from "tmp-promise"
 import fs from "fs"
+import tmp from "tmp-promise"
 
 export const patch = async (context: Context, cwd: string = process.cwd()): Promise<Context> => {
-  await withDir(async ({ path }) => {
-    await sync({
-      from: { fs: fsFromVolume(context.volume), path: "/" },
-      to: { fs, path },
-    })
-    spawn("git", ["diff", "--no-index", "--patch", cwd, path], { stdio: "inherit" })
+  const dir = await tmp.dir({ unsafeCleanup: true })
+
+  await sync({
+    from: { fs: fsFromVolume(context.volume), path: "/" },
+    to: { fs, path: dir.path },
   })
-  return context
+
+  return new Promise((resolve, reject) => {
+    spawn("git", ["diff", "--no-index", "--patch", cwd, dir.path], { stdio: "inherit" }).on("exit", () => {
+      dir.cleanup()
+      resolve(context)
+    })
+  })
 }
