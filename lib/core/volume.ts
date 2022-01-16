4787b2d4992d4ffc5f6d61546394cb161e18dded
Louis Quinnell <_@ziorobo.co>
2022-01-16 13:54:06 +1100

    Remove previous effort

--- lib/core/volume.ts
+++
@@ -1,8 +0,0 @@
-import * as memfs from "memfs"
-
-export type VolumeInstance = InstanceType<typeof memfs.Volume>
-export const Volume = memfs.Volume
-
-export const fsFromVolume = function (volume: VolumeInstance) {
-  return memfs.createFsFromVolume(volume) as unknown as typeof import("fs")
-}
