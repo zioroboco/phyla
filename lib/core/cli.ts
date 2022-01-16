d005a2fd06a68aff00fa6909baae08a98b20f44a
Louis Quinnell <_@ziorobo.co>
2022-01-16 19:30:39 +1100

    Add run command stub

--- lib/core/cli.ts
+++ lib/core/cli.ts
@@ -2,18 +2,43 @@ import { Command, Option } from "clipanion"
 import { inspect } from "util"
 import { resolve } from "path"
 
-export class ShowConfigCommand extends Command {
-  static paths = [["show-config"]]
+enum Category {
+  Main = "main",
+  Util = "util",
+}
+
+const importConfig = async function (configPath?: string) {
+  return import(resolve(configPath ?? ".begatrc.mjs")).then(
+    module => module.default
+  )
+}
+
+export class RunCommand extends Command {
+  static paths = [["run"]]
   static usage = Command.Usage({
-    category: "util",
-    description: "Write config to stdout.",
+    category: Category.Main,
+    description: `Run pipeline configured at path (default: ".")`,
   })
 
   config = Option.String({ name: "path", required: false })
 
   async execute () {
-    const config = await import(resolve(this.config ?? ".begatrc.mjs"))
-    this.context.stdout.write(inspect(config.default))
+    const config = await importConfig(this.config)
+  }
+}
+
+export class ShowConfigCommand extends Command {
+  static paths = [["config"]]
+  static usage = Command.Usage({
+    category: Category.Util,
+    description: `Write config at path to stdout (default: ".")`,
+  })
+
+  config = Option.String({ name: "path", required: false })
+
+  async execute () {
+    const config = await importConfig(this.config)
+    this.context.stdout.write(inspect(config))
     process.exit(0)
   }
 }
