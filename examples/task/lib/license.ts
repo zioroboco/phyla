9b5c0ac007010866fb9552e517cfc245d171d72d
Louis Quinnell <_@ziorobo.co>
2022-01-16 21:03:07 +1100

    Re-implement package.json updates in the example task

--- examples/task/lib/license.ts
+++ examples/task/lib/license.ts
@@ -1,4 +1,5 @@
 import { Task } from "begat"
+import { join } from "path"
 
 type Options = {
   license: "MIT"
@@ -7,14 +8,24 @@ type Options = {
 
 export const license: Task<Options> = {
   implementation: async (context, options) => {
+    // TODO: move me to the "before" suite
     if (options.license !== "MIT") {
       throw new Error("Only the MIT license is supported")
     }
 
-    // @ts-ignore
-    console.log({
-      context,
-      options,
-    })
+    const fs = context.fs.promises
+    const cwd = context.cwd
+
+    const packageJson = JSON.parse(
+      await fs.readFile(join(cwd, "package.json"), "utf8")
+    )
+
+    packageJson.author = options.author
+    packageJson.license = options.license
+
+    await fs.writeFile(
+      join(cwd, "package.json"),
+      JSON.stringify(packageJson, null, 2) + "\n"
+    )
   },
 }
