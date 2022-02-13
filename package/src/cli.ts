7fc1116fbab92e563f04ed96be3294b1dd67def7
Louis Quinnell <_@ziorobo.co>
2022-02-13 20:23:56 +1100

    Remove unecessary direct dependencies

--- package/src/cli.ts
+++ package/src/cli.ts
@@ -1,11 +1,8 @@
 import * as fs from "fs/promises"
 import * as path from "path"
-import * as tmp from "tmp-promise"
 import { Command } from "clipanion"
 import { Config, run } from "begat/core"
-import { execa } from "execa"
 import { inspect } from "util"
-import { watch } from "chokidar"
 
 enum Category {
   Main = "main",
@@ -16,48 +13,6 @@ const importConfig = async function () {
   return import(path.join(process.cwd(), ".begatrc.mjs")).then(
     module => module.default
   )
-}
-
-export class DevCommand extends Command {
-  static paths = [["dev"]]
-  static usage = Command.Usage({
-    category: Category.Main,
-  })
-
-  async execute () {
-    const { cleanup, path: tmpdir } = await tmp.dir()
-
-    const workspace = {
-      dir: path.join(tmpdir, "workspace"),
-      file: path.join(tmpdir, "begat.code-workspace"),
-    }
-
-    const config = {
-      folders: [
-        { path: workspace.dir },
-      ],
-    }
-
-    await fs.mkdir(workspace.dir)
-    await fs.writeFile(
-      path.join(workspace.file),
-      JSON.stringify(config)
-    )
-
-    await fs.writeFile(
-      path.join(workspace.dir, "package.json"),
-      JSON.stringify({
-        name: "derp",
-        version: "69",
-      })
-    )
-
-    await execa("code", ["--new-window", workspace.file])
-
-    setTimeout(() => {
-      cleanup()
-    }, 100000)
-  }
 }
 
 export class WriteCommand extends Command {
