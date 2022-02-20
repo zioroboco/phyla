b258ea5d8cff21b6062a4699306e79abbef8cf15
Louis Quinnell <_@ziorobo.co>
2022-02-20 16:09:28 +1100

    Add an example install task using zx

--- examples/project/phyla.mjs
+++ examples/project/phyla.mjs
@@ -2,7 +2,8 @@ import { config } from "@phyla/core"
 
 export default config({
   pipeline: [
-    import("@phyla/example-task"),
+    import("@phyla/example-task-install"),
+    import("@phyla/example-task-license"),
   ],
   parameters: {
     author: "Raymond Luxury-Yacht <rayly@example.com>",
---
+++ examples/task-install/src/install.ts
@@ -0,0 +1,22 @@
+import * as path from "path"
+
+import { $, cd } from "zx"
+import { task } from "@phyla/core"
+import expect from "expect"
+
+export default task(() => ({
+  run: async ctx => {
+    await cd(ctx.cwd)
+    await $`echo "# replace me with a real install!" > pnpm-lock.yaml`
+  },
+
+  post: ({ it }, { cwd, fs }) => [
+    it(`worked`, async () => {
+      const lockfile = await fs.promises.readFile(
+        path.join(cwd, "pnpm-lock.yaml"),
+        "utf8"
+      )
+      expect(lockfile).toMatch("install")
+    }),
+  ],
+}))
