4eb81d974412810a0dedcdb6c0d0964880d0ec54
Louis Quinnell <_@ziorobo.co>
2022-03-06 08:40:38 +1100

    Set up wallaby

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
-import { createRequire } from "module"
-
-const require = createRequire(import.meta.url)
 const { compilerOptions } = require("./tsconfig.json")
 
 /**
  * @type {import("@jest/types").Config.InitialOptions}
  */
-export const config = {
+const config = {
   testMatch: ["**/*.spec.ts"],
   preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
@@ -16,11 +13,10 @@ export const config = {
   globals: {
     "ts-jest": {
       isolatedModules: true,
-      diagnostics: { ignoreCodes: [151001] },
       useESM: true,
       tsconfig: compilerOptions,
     },
   },
 }
 
-export default config
+module.exports = config
