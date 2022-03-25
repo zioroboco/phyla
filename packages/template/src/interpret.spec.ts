228f1fca806310f47288b77a0322274622131159
Louis Quinnell <_@ziorobo.co>
2022-03-25 22:37:13 +1100

    Interpret simple template variables

--- packages/template/src/interpret.spec.ts
+++ packages/template/src/interpret.spec.ts
@@ -1,7 +1,112 @@
-import { expect, it } from "@jest/globals"
+import { describe, expect, it } from "@jest/globals"
 import { interpret } from "./interpret.js"
 
-it(`returns templates without variables verbatim`, () => {
-  const result = interpret(`blah\nblah`)
-  expect(result).toMatch(`blah\nblah`)
+describe(`expecting no template variables`, () => {
+  const template = `blah blah`
+
+  it(`returns the template verbatim`, () => {
+    expect(interpret(template)).toMatchObject({
+      right: template })
+  })
+
+  describe(`when provided variables`, () => {
+    const variables = { bee: "🐝" }
+
+    it(`ignores any variables and returns the template verbatim`, () => {
+      expect(interpret(template, variables)).toMatchObject({
+        right: template,
+      })
+    })
+  })
+})
+
+describe(`expecting a single template variable`, () => {
+  const template = `blah {{bee}} blah`
+
+  describe(`when provided a value for the variable`, () => {
+    const variables = { bee: "🐝" }
+
+    it(`returns the template with the value interpolated`, () => {
+      expect(interpret(template, variables)).toMatchObject({
+        right: `blah 🐝 blah`,
+      })
+    })
+  })
+
+  describe(`when the variable is not defined`, () => {
+    const variables = {}
+
+    it(`returns an error including the missing variable name`, () => {
+      expect(interpret(template, variables)).toMatchObject({
+        left: [
+          expect.objectContaining({
+            name: "ReferenceError",
+            message: "bee is not defined",
+          }),
+        ],
+      })
+    })
+  })
+})
+
+describe(`expecting a single template variable (including whitespace)`, () => {
+  const template = `blah {{ bee }} blah`
+
+  describe(`when provided a value for the variable`, () => {
+    const variables = { bee: "🐝" }
+
+    it(`returns the template with the value interpolated`, () => {
+      expect(interpret(template, variables)).toMatchObject({
+        right: `blah 🐝 blah`,
+      })
+    })
+  })
+
+  describe(`when the variable is not defined`, () => {
+    const variables = {}
+
+    it(`returns an error including the missing variable name`, () => {
+      expect(interpret(template, variables)).toMatchObject({
+        left: [
+          expect.objectContaining({
+            name: "ReferenceError",
+            message: "bee is not defined",
+          }),
+        ],
+      })
+    })
+  })
+})
+
+describe(`expecting multiple template variables`, () => {
+  const template = `{{bee}} {{ladybird}}`
+
+  describe(`when provided values for all variables`, () => {
+    const variables = {
+      bee: "🐝",
+      ladybird: "🐞",
+    }
+
+    it(`returns the template with all values interpolated`, () => {
+      const result = interpret(template, variables)
+      expect(result).toMatchObject({
+        right: `🐝 🐞`,
+      })
+    })
+  })
+
+  describe(`when all variables are not defined`, () => {
+    const variables = {}
+
+    it(`returns errors including the names of all missing variables`, () => {
+      const result = interpret(template, variables)
+      expect(result).toMatchObject({
+        left: ["bee", "ladybird"].map(key =>
+          expect.objectContaining({
+            name: "ReferenceError",
+            message: `${key} is not defined`,
+          })),
+      })
+    })
+  })
 })
--- packages/template/src/interpret.ts
+++ packages/template/src/interpret.ts
@@ -1,3 +1,29 @@
-export function interpret (template: string, variables?: object) {
-  return template
+import * as E from "fp-ts/Either"
+
+export function interpret (
+  template: string,
+  variables: { [key: string]: string } = {}
+): E.Either<Error[], string> {
+  const errors: Error[] = []
+
+  const definitions = Object.keys(variables).map(
+    key => `let ${key} = ${JSON.stringify(variables[key])}`
+  )
+
+  const rendered = template.replaceAll(/{{[ ]*(.+?)[ ]*}}/g, (_, capture) => {
+    try {
+      return new Function(
+        [...definitions, `return ${capture}`].join(";")
+      )()
+    } catch (e) {
+      errors.push(e instanceof Error ? e : new Error(String(e)))
+      return ""
+    }
+  })
+
+  if (errors.length > 0) {
+    return E.left(errors)
+  } else {
+    return E.right(rendered)
+  }
 }
