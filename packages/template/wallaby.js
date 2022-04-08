e4d95e98c6816b4c5d589efb3a79835442c529e8
Louis Quinnell <_@ziorobo.co>
2022-04-09 00:08:48 +1000

    Restore the template package

---
+++ packages/template/wallaby.js
@@ -0,0 +1,34 @@
+export default function () {
+  return {
+    files: [
+      "./package.json",
+      "./tsconfig.json",
+      "./src/**/*.{ts,json}",
+      "!./src/**/*.spec.ts",
+      "!./node_modules/**/*",
+    ],
+
+    tests: [
+      "./src/**/*.spec.ts",
+    ],
+
+    runMode: "onsave",
+
+    env: {
+      type: "node",
+      runner: "/usr/local/bin/node",
+      params: {
+        runner: [
+          "--experimental-import-meta-resolve",
+          "--experimental-specifier-resolution=node",
+          "--no-warnings",
+        ].join(" "),
+      },
+    },
+
+    // see: https://wallabyjs.com/docs/integration/esm.html#mocha
+    testFramework: "mocha",
+    symlinkNodeModules: true,
+    workers: { restart: true },
+  }
+}
